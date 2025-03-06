4ea82c1e57bb81a67013bc8f7a7cbc0427974152
Eduardo gomara sagastume <eduardo.gomara.sagastume@gmail.com>
2025-03-06 14:35:20 -0500

    added sse and queue flow

--- app.js
+++ app.js
@@ -2,8 +2,8 @@ import express from "express";
 import path from "path";
 import { fileURLToPath } from "url";
 import { dirname } from "path";
-import process_website from "./lib/website.js";
 
+import { clients, queue } from "./lib/queue.js";
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
 const app = express();
@@ -12,6 +12,7 @@ const port = 3000;
 // Middleware to parse URL-encoded data and JSON data
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
+app.use(express.static(path.join(__dirname, 'public')));
 
 // Serve index.html
 app.use(express.static(path.join(__dirname, "views")));
@@ -20,27 +21,29 @@ app.get("/", (req, res) => {
   res.sendFile(path.join(__dirname, "views", "index.html"));
 });
 
+app.get('/job_status/:id', (req, res) => {
+  // Set headers to keep the connection alive and tell the client we're sending event-stream data
+  res.setHeader('Content-Type', 'text/event-stream');
+  res.setHeader('Cache-Control', 'no-cache');
+  res.setHeader('Connection', 'keep-alive');
+  let jobId = req.params.id
+  clients.set(jobId, res )
+  req.on('close', () => {
+      res.end();
+  });
+});
+
 // POST /export-webflow
 app.post("/export-webflow", async (req, res) => {
   try {
-    const { page } = req.body; // Read from JSON body
+    const { page } = req.body; 
     if (!page) {
       return res.status(400).json({ error: "Missing parameters" });
     }
-
-    const start = performance.now(); // Start time
-
-    // 1) Call our function to get the ZIP as a buffer
-    const zipBuffer = await process_website(page);
-
-    // 2) Encode the buffer as base64
-    const base64Zip = zipBuffer.toString("base64");
-
-    const end = performance.now(); // End time
-    console.log(`Time taken: ${(end - start).toFixed(2)} ms`);
-
-    // 3) Return JSON with the base64 content
-    res.json({ zipFile: base64Zip });
+    const job =await queue.add({
+      page
+    })
+    res.json({ jobId: job.id });
   } catch (error) {
     console.error("Export error:", error);
     return res.status(500).json({ error: "Error creating zip file" });
